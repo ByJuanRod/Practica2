bb52a71ed183c1babe34e77e49f47d8c0cdc8c3e
anton <juanrodriguezz2689@gmail.com>
2025-09-25 16:26:02 -0400

    Problema II parte 2

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,24 @@ import logica.Utilidades;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        List<Estudiante> lista = ordenarMergeSort(Utilidades.generarEstudiantes(10),false);
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Ingrese la cantidad de estudiantes: ");
+        int n = sc.nextInt();
+
+        System.out.println("Seleccione el metodo de ordenamiento (desc/asc): ");
+        String ordenamiento = sc.next();
+
+        boolean orden = ordenamiento.equalsIgnoreCase("asc");
+
+        List<Estudiante> lista = ordenarMergeSort(Utilidades.generarEstudiantes(n),orden);
         for(Estudiante estudiante : lista){
             System.out.println(estudiante);
         }
